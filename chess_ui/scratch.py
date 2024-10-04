a93d7b77047e5eb3ac4e172f8874f198fbae7344
tejaschaudhari6402 <69662790+tejaschaudhari6402@users.noreply.github.com>
2024-10-03 21:07:17 -0500

    Update scratch.py

--- chess_ui/scratch.py
+++ chess_ui/scratch.py
@@ -5,5 +5,6 @@ puzzles_df = pd.read_csv("C:\\Users\\wei0c\\Desktop\\school\\7-1\\CS-6320-NLP\\l
 fen = puzzles_df.iloc[0]['FEN']
 moves = puzzles_df.iloc[0]['Moves']
 
+
 print(moves)
 print(type(moves))
