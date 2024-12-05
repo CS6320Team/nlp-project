527ab5ff391b9d4f4514bede6b338d375851e466
Zenotick <54344677+Zenotick@users.noreply.github.com>
2024-12-05 17:40:50 -0600

    updated gitignore config example.env and added some guard rails

--- chess_ui/main.py
+++ chess_ui/main.py
@@ -14,11 +14,27 @@ def main():
     load_dotenv()
 
     app = QApplication(sys.argv)
-    puzzle_data = pd.read_csv('lichess_puzzles.csv.zst')
+    open_api_key = os.getenv("OPENAI_API_KEY")
+    if open_api_key is None:
+        raise ValueError("OPENAI_API_KEY environment variable is not set")
+
+    puzzle_path = os.getenv("LICHESS_PUZZLE_DATASET_PATH")
+    if puzzle_path is None or not os.path.exists(puzzle_path):
+        raise ValueError("LICHESS_PUZZLE_DATASET_PATH environment variable is not set")
+
+    stockfish_path = os.getenv("STOCKFISH_PATH")
+    if stockfish_path is None or not os.path.exists(stockfish_path):
+        raise ValueError("STOCKFISH_PATH environment variable is not set")
+
+    openai_model = os.getenv("OPENAI_MODEL")
+    if openai_model is None:
+        raise ValueError("OPENAI_MODEL environment variable is not set")
+
+    puzzle_data = pd.read_csv(puzzle_path)
     coach = ChessCoach(
-        stockfish=Stockfish(r"C:\stockfish\stockfish-windows-x86-64-avx2.exe"),
-        openai_api_key=os.getenv("OPENAI_API_KEY"),
-        openai_model="gpt-4o-mini",
+        stockfish=Stockfish(stockfish_path),
+        openai_api_key=open_api_key,
+        openai_model=openai_model,
         chess_model="Waterhorse/chessgpt-chat-v1"
     )
     home_page = ChessHomePageUI(puzzle_data, coach)
